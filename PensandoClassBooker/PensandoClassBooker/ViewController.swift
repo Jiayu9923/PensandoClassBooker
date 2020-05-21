53c7ba74af6b99b8879a03ecf77c168423a260c3
Jiayu9923 <jiayu9923@gmail.com>
2020-05-21 11:54:33 +0800

    TabBar and navigation bar

--- PensandoClassBooker/PensandoClassBooker/ViewController.swift
+++ PensandoClassBooker/PensandoClassBooker/ViewController.swift
@@ -12,7 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
